273b5252e68b0463ce8ab0cae191d22103e8a913
mrex5 <129540281+mrex5@users.noreply.github.com>
2023-07-23 21:35:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// Gerekli güz ve derinlik ayarları
+
+// Temel JavaScript örnekleri - Dilediğiniz gibi düzenleyebilirsiniz
+const messageContainer = document.querySelector(".message-container");
+const messageInput = document.getElementById("messageInput");
+const sendButton = document.getElementById("sendButton");
+
+sendButton.addEventListener("click", () => {
+    const message = messageInput.value.trim();
+    if (message !== "") {
+        addMessageToContainer(message);
+        messageInput.value = "";
+    }
+});
+
+function addMessageToContainer(message) {
+    const messageElement = document.createElement("div");
+    messageElement.textContent = message;
+    messageContainer.appendChild(messageElement);
+    scrollToBottom();
+    // Mesajları burada göndermek istediğiniz başka bir platforma da kaydedebilirsiniz.
+}
+
+function scrollToBottom() {
+    // Mesajlar bittiğinde otomatik olarak aşağı kaydırma
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
