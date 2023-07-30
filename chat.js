fe6e585ce95b20270f0ab1c37fa284e9fd90bf91
mrex5 <129540281+mrex5@users.noreply.github.com>
2023-07-31 01:27:39 +0300

    Delete chat.js

--- chat.js
+++
@@ -1,9 +0,0 @@
-const chatBox = document.getElementById("chat-box");
-const chatInput = document.getElementById("chat-input");
-const chatSendButton = document.getElementById("chat-send");
-
-chatSendButton.addEventListener("click", () => {
-    const message = chatInput.value;
-    chatBox.innerHTML += `<p>${message}</p>`;
-    chatInput.value = "";
-});
